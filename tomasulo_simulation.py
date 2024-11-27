8b69a57971edb1b3fada690ffdfdadd0fe13f506
Ke11en-Anderson <kellenanderson1234@gmail.com>
2024-11-26 22:15:40 -0600

    Add rough pseudocode

---
+++ tomasulo_simulation.py
@@ -0,0 +1,14 @@
+# Read in relevant data from the text file and use the data to populate variables, lists, etc to be used in the simulation
+
+
+# Create a class which is something like 'InstructionRecord' with all of the relevant members such as opcode, source_reg, etc. and a basic constructor
+
+
+# Make the logic for the simulation
+# Initialize each instruction record and put them in the queue
+# Initialize all containers that we need such as reservation stations and execution units (probably can just be lists)
+# Then issue, dispatch, broadcast etc.
+# Make sure we are keeping track of all the data we need for the output file
+
+
+# Create output file assuming we have all the data we need
