875a1251833f4cfddde9352760811350a7458436
graciegorka <gracie82003@gmail.com>
2024-11-30 20:34:47 -0600

    Adding dispatch definition

--- tomasulo_simulation.py
+++ tomasulo_simulation.py
@@ -38,8 +38,9 @@ def main():
     reservation_stations = add_sub_reservations + mul_div_reservations
     RAT = [None] * 8
     num_instructions, num_cycles, instruction_queue, RF = read_text_file()
-    for cycle in num_cycles:
+    for instruction in range(num_instructions):
         issue_instructions(instruction_queue, reservation_stations, RAT, RF)
+        dispatch_instructions(reservation_stations)
 
 # Read in relevant data from the text file and use the data to populate variables, lists, etc to be used in the simulation
 def read_text_file():
@@ -66,38 +67,48 @@ def read_text_file():
             line_index += 1
     return num_instructions, num_cycles, instruction_queue, RF
 
+#Makes sure an RS is open and assigns variables to the RS
 def issue_instructions(instruction_queue, reservation_stations, RAT, RF):
     if not instruction_queue:
         return
     instruction = instruction_queue[0]
+    
     # Find an available reservation station
     rs = None
     for station in reservation_stations:
         if not station.busy and (
             (instruction.opcode in ADD_SUB_OPCODES and "RS1" <= station.name <= "RS3")
-            or (instruction.opcode in MUL_DIV_OPCODES and "RS4" <= station.name <= "RS5")
-        ):
+            or (instruction.opcode in MUL_DIV_OPCODES and "RS4" <= station.name <= "RS5")):
             rs = station
             break
+
     #If RS is found, update the rs
     if rs:
         rs.busy = True
         rs.op = instruction.opcode
         rs.vj, rs.qj = (
             (RF[instruction.source1], None) if RAT[instruction.source1] is None 
-            else (None, RAT[instruction.source1])
-        )
+            else (None, RAT[instruction.source1]))
         rs.vk, rs.qk = (
             (RF[instruction.source2], None) if RAT[instruction.source2] is None 
-            else (None, RAT[instruction.source2])
-        )
-        
+            else (None, RAT[instruction.source2]))
+
          # Update the RAT to point to the reservation station
         RAT[instruction.destination] = rs.name
 
         # Remove the instruction from the instruction queue
         instruction_queue.pop(0)
-  
+
+#Makes sure all conditions are met, marks insturction as dispatched and sets the run time
+def dispatch_instructions(reservation_stations):
+    for rs in reservation_stations:
+        if rs.busy and rs.dispatched==False and rs.qj is None and rs.qk is None:
+            rs.dispatched = True
+            rs.time_remaining = (
+                ADD_TIME if rs.op in ADD_SUB_OPCODES 
+                else MUL_TIME if rs.op == 2 
+                else DIV_TIME
+            )
 
 # Create output file assuming we have all the data we need
 def output_simulation_result():
